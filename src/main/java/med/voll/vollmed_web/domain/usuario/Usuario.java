e954e66f3a0684fa273374d69882d3d689b35ca5
Renato28 <renatonobrega87@gmail.com>
2024-10-15 13:56:58 -0300

    implementando a classe Usuario e adicionando dependencia do spring security

---
+++ src/main/java/med/voll/vollmed_web/domain/usuario/Usuario.java
@@ -0,0 +1,34 @@
+package med.voll.vollmed_web.domain.usuario;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
