fa614b02332b090165f4e0a4cf07272651ffc6be
Renato28 <renatonobrega87@gmail.com>
2024-10-15 10:39:04 -0300

    implementando a interface MedicoRepository

---
+++ src/main/java/med/voll/vollmed_web/domain/MedicoRepository.java
@@ -0,0 +1,20 @@
+package med.voll.vollmed_web.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+    @Query("""
+            SELECT
+                CASE WHEN COUNT(m) > 0 THEN TRUE ELSE FALSE END
+            FROM
+                Medico m
+            WHERE (m.email = :email OR m.crm = :crm) AND (:id IS NULL OR m.id <> :id)
+            """)
+    boolean isJaCadastrado(String email, String crm, Long id);
+
+    List<Medico> findByEspecialidade(Especialidade especialidade);
+}
