ed32e1535c20c4bb3a60520d5d27a56e2e524610
Renato28 <renatonobrega87@gmail.com>
2024-10-15 13:58:32 -0300

    implementando a interface UsuarioRepository

---
+++ src/main/java/med/voll/vollmed_web/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package med.voll.vollmed_web.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmailIgnoreCase(String email);
+}
