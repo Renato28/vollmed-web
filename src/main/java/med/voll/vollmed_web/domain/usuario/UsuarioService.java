0ef494ca45a8542740dc689a3d9129dba6f06a9f
Renato28 <renatonobrega87@gmail.com>
2024-10-15 14:01:26 -0300

    implementando a classe UsuarioService

---
+++ src/main/java/med/voll/vollmed_web/domain/usuario/UsuarioService.java
@@ -0,0 +1,22 @@
+package med.voll.vollmed_web.domain.usuario;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmailIgnoreCase(username).orElseThrow(() ->
+                new UsernameNotFoundException("Usuário não foi encontrado!"));
+    }
+}
